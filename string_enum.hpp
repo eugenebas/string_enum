72e0a53ac419369c340618cf6bd330e3b91680b5
Yevgeniy Basargin <19927545+eugenebas@users.noreply.github.com>
2023-04-12 23:46:38 +0300

    Added missing include guard into string_enum.hpp + renamed get_nth_arg macro according to common style

--- string_enum.hpp
+++ string_enum.hpp
@@ -1,3 +1,6 @@
+#ifndef __STRING_ENUM_HPP__
+#define __STRING_ENUM_HPP__
+
 #include <string>
 #include <type_traits>
 #include <compare>
@@ -8,13 +11,11 @@
 #include "generate_constants.hpp"
 #include <array>
 
-#define __STR_ENUM_STRING_ARRAY_START(enum_name, ...) constexpr std::array<const char*, __STR_ENUM_COUNT_ARGS(__VA_ARGS__) > __STR_ENUM__INNER_ARRAY_NAME(enum_name) {
-#define __STR_ENUM_STRING_ARRAY_END(enum_name) }; // __STR_ENUM_STRING_ARRAY_START(enum_name)
 
-#define __STR_ENUM_STRING_ARRAY(enum_name, ...)\
-    __STR_ENUM_STRING_ARRAY_START(enum_name, __VA_ARGS__)\
-    __STR_ENUM_GENERATE_STRINGS(__VA_ARGS__)\
-    __STR_ENUM_STRING_ARRAY_END(enum_name)\
+#define __STR_ENUM__STRING_ARRAY(enum_name, ...)\
+    constexpr std::array<const char*, __STR_ENUM_COUNT_ARGS(__VA_ARGS__) > __STR_ENUM__INNER_ARRAY_NAME(enum_name) {\
+        __STR_ENUM_GENERATE_STRINGS(__VA_ARGS__)\
+    };
 
 
 #define __STR_ENUM_INNER_ENUM(enum_name, ...) enum class __STR_ENUM__INNER_ENUM_CLASS_NAME(enum_name) {__VA_ARGS__};
@@ -22,7 +23,7 @@
 #define STRING_ENUM(enum_name, ...) \
 struct __STR_ENUM__BASE_STRING_ENUM_NAME(enum_name) { \
     __STR_ENUM_INNER_ENUM(enum_name, __VA_ARGS__)\
-    static __STR_ENUM_STRING_ARRAY(enum_name, __VA_ARGS__)\
+    static __STR_ENUM__STRING_ARRAY(enum_name, __VA_ARGS__)\
     \
     constexpr __STR_ENUM__BASE_STRING_ENUM_NAME(enum_name)(__STR_ENUM__INNER_ENUM_CLASS_NAME(enum_name) value): mValue(value){}\
     constexpr __STR_ENUM__BASE_STRING_ENUM_NAME(enum_name)(const __STR_ENUM__BASE_STRING_ENUM_NAME(enum_name)& other) = default;\
@@ -43,3 +44,4 @@ struct enum_name : public __STR_ENUM__BASE_STRING_ENUM_NAME(enum_name) { \
     __STR_ENUM__GENERATE_CONSTANTS(enum_name, __VA_ARGS__) \
 };
 
+#endif // __STRING_ENUM_HPP__
