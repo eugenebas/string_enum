1bb68d0b0a909d84c76a5f06836d12f360b6f5b6
Evgeny Basargin <19927545+eugenebas@users.noreply.github.com>
2024-12-15 12:08:41 +0300

    Added test for count_args macro

--- tests.cpp
+++ tests.cpp
@@ -64,3 +64,9 @@ TEST(string_enum, switchExpressionSupport) {
         default: FAIL();
     }
 }
+
+TEST(count_args, count_args) {
+    EXPECT_EQ(__STR_ENUM__COUNT_ARGS(A, B, C), 3);
+    EXPECT_EQ(__STR_ENUM__COUNT_ARGS(A), 1);
+    EXPECT_EQ(__STR_ENUM__COUNT_ARGS(A, B, C, D, E, F, G), 7);
+}
